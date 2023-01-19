4b741ef3065d0e2017cc9268e0eb324cbebf3369
kamlesh <kamlesh.kumawat@axa-abs-co.in>
2023-01-20 01:13:14 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import {useState} from 'react'
+// import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [word, setWord] = useState("");
+  const [rhymeList, setRhymeList] = useState([]);
+
+  function getRhymes() {
+    fetch("https://api.datamuse.com/words?rel_rhy=" + word.toLowerCase())
+      .then((res) => {
+        return res.json();
+      })
+      .then((res) => {
+        setRhymeList(res);
+      });
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="inputContainer">
+        <div className="prompt">Type a word to rhyme!</div>
+        <input
+          onChange={(event) => {
+            event.preventDefault();
+            setWord(event.target.value);
+          }}
+          className="input"
+        />
+        <button onClick={getRhymes} className="button" type="button">
+          Rhyme word
+        </button>
+      </div>
+      <div className="listContainer">
+        <div className="successContainer">
+          <div className="successText">
+            {rhymeList.length === 0
+              ? "Type a valid word"
+              : `Found ${rhymeList.length} matching words!!!`}
+          </div>
+        </div>
+        {rhymeList.map((rhyme, i) => (
+          <div className="row">
+            <div className="rowText">{rhyme.word}</div>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
